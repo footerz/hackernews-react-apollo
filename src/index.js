1c0b1588365c4ae7729ba9d08e4e04e3fb5346bb
Footerz <dan.footman@river-island.com>
2019-04-03 11:11:11 +0100

    Finish - Mutations: Creating Links

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-// import serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
@@ -23,4 +23,4 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById('root')
 )
-// serviceWorker()
+serviceWorker.unregister();
